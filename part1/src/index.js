3e9d9311ce394cec9bc5ce4ec8cd62c2743723ed
Xarri Jorge <kodinglife@gmail.com>
2019-09-19 22:31:23 +0000

    Moved variables to array of objects

--- part1/src/index.js
+++ part1/src/index.js
@@ -33,13 +33,22 @@ const Total = (props) => {
 }
 
 const App = () => {
-    const course = 'Half Stack application development';
-    const part1 = 'Fundamentals of React';
-    const exercises1 = 10;
-    const part2 = 'Using props to pass data';
-    const exercises2 = 7;
-    const part3 = 'State of a component';
-    const exercises3 = 14;
+    const course = 'Half Stack application development'
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ];
+
 
     return (
         <>
