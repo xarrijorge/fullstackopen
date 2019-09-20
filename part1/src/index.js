f5331208e287e83be45a5d899b63b2d2f45cfd4c
Xarri Jorge <kodinglife@gmail.com>
2019-09-20 22:47:51 +0000

    restructured code and used array destructing to assign variables

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,62 +1,66 @@
-import React, { Fragment } from 'react'
-import ReactDOM from 'react-dom'
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 
-const Header = (props) => {
-    return (
-        <h1>{props.course}</h1>
-    )
-}
+const Header = props => {
+  return <h1>{props.course}</h1>;
+};
 
-const Part = (props) => {
-    return (
-        <p>
-            {props.part} {props.exercise}
-        </p>
-    )
-}
+const Part = props => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  );
+};
 
-const Content = (props) => {
-    return (
-        <Fragment>
-            <Part part={props.part1} exercise={props.exercises1} />
-            <Part part={props.part2} exercise={props.exercises2} />
-            <Part part={props.part3} exercise={props.exercises3} />
-        </Fragment>
+const Content = props => {
+  const objStore = props.objStore;
+  const { name: name1, exercises: exer1 } = objStore[0];
+  const { name: name2, exercises: exer2 } = objStore[1];
+  const { name: name3, exercises: exer3 } = objStore[2];
+  return (
+    <Fragment>
+      <Part name={name1} exercises={exer1} />
+      <Part name={name2} exercises={exer2} />
+      <Part name={name3} exercises={exer3} />
+    </Fragment>
+  );
+};
 
-    )
-}
-
-const Total = (props) => {
-    return (
-        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    )
-}
+const Total = props => {
+  const store = props.store;
+  const [exer1, exer2, exer3] = [
+    store[0].exercises,
+    store[1].exercises,
+    store[2].exercises,
+  ];
+  return <p>Number of exercises {exer1 + exer2 + exer3}</p>;
+};
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ];
+  const course = 'Half Stack application development';
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
 
+  return (
+    <>
+      <Header course={course} />
+      <Content objStore={parts} />
+      <Total store={parts} />
+    </>
+  );
+};
 
-    return (
-        <>
-            <Header course={course} />
-            <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-            <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-        </>
-    )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
