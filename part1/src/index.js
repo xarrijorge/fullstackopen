fa11b9b981be11e92db5f26a140ac1e4acaf9607
Xarri Jorge <kodinglife@gmail.com>
2019-09-20 23:00:10 +0000

    Changed content to an object and fixed broken variable calls

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
 const Header = props => {
-  return <h1>{props.course}</h1>;
+  return <h1>{props.course.name}</h1>;
 };
 
 const Part = props => {
@@ -14,10 +14,10 @@ const Part = props => {
 };
 
 const Content = props => {
-  const parts = props.parts;
-  const { name: name1, exercises: exer1 } = parts[0];
-  const { name: name2, exercises: exer2 } = parts[1];
-  const { name: name3, exercises: exer3 } = parts[2];
+  const course = props.course.course;
+  const { name: name1, exercises: exer1 } = course[0];
+  const { name: name2, exercises: exer2 } = course[1];
+  const { name: name3, exercises: exer3 } = course[2];
   return (
     <Fragment>
       <Part name={name1} exercises={exer1} />
@@ -28,37 +28,38 @@ const Content = props => {
 };
 
 const Total = props => {
-  const parts = props.parts;
+  const course = props.course.course;
   const [exer1, exer2, exer3] = [
-    parts[0].exercises,
-    parts[1].exercises,
-    parts[2].exercises,
+    course[0].exercises,
+    course[1].exercises,
+    course[2].exercises,
   ];
   return <p>Number of exercises {exer1 + exer2 + exer3}</p>;
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ];
-
+  const course = {
+    name: 'Half Stack application development',
+    course: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </>
   );
 };
