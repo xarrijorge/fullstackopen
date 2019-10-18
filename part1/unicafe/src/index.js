78cbcf202332cf99b375f51d011039c3d90de8a9
Xarri Jorge <kodinglife@gmail.com>
2019-10-18 21:28:10 +0000

    Moved stats to table and fixed errors

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,13 +2,16 @@ import React, { Fragment, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Statistic = ({ text, value }) => {
+const Statistic = ({ text, value, key }) => {
   return (
-    <ul>
-      <li>
-        {text} <span>{value}</span>
-      </li>
-    </ul>
+    <table>
+      <tbody>
+        <tr>
+          <td>{text}</td>
+          <td>{value}</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
@@ -23,7 +26,7 @@ const App = () => {
   const [stats, setStats] = useState([]);
   const all = stats.length;
   const avg = (good * 1 + bad * -1) / all || 0;
-  const perc = (good * 100) / all || 0;
+  const perc = `${(good * 100) / all || 0} %`;
 
   const setToGood = () => {
     setStats(stats.concat('G'));
@@ -45,13 +48,20 @@ const App = () => {
       <Button text="Neutral" handleClick={() => setToNeutral()} />
       <Button text="Bad" handleClick={() => setToBad()} />
       <h2>Statistics</h2>
-      <Statistic text="Good" value={good} />
-      <Statistic text="Neutral" value={neutral} />
-      <Statistic text="Bad" value={bad} />
-      <Statistic text="All" value={all} />
-      <Statistic text="Average" value={avg} />
-      <Statistic text="Percentage" value={perc} />
+      {stats.length > 0 ? (
+        <>
+          <Statistic text="Good" value={good} />
+          <Statistic text="Neutral" value={neutral} />
+          <Statistic text="Bad" value={bad} />
+          <Statistic text="All" value={all} />
+          <Statistic text="Average" value={avg} />
+          <Statistic text="Positive" value={perc} />
+        </>
+      ) : (
+        'No feedback given'
+      )}
     </Fragment>
   );
 };
+ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
