be9e08bde7371abbad651bb0f7c0e9f01c9c92b7
Xarri Jorge <kodinglife@gmail.com>
2019-10-24 13:20:23 +0000

    commit all changes before PC dies

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,33 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = props => {
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-  ];
-  let rand = Math.floor(Math.random() * anecdotes.length);
-  const [selected, setSelected] = useState(rand);
-  const [message, setMessage] = useState('');
+const Button = props => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
 
-  const updateMessage = () => {
-    setSelected(rand);
-  };
+const App = props => {
+  const [selected, setSelected] = useState(0);
+  const [message, setMessage] = useState('');
+  const [vote, setVote] = useState(0);
+  let points = new Uint8Array(props.anecdotes.length);
+  const [copy, setCopy] = useState([...points]);
+  let rand = Math.floor(Math.random() * props.anecdotes.length);
 
   useEffect(() => {
-    setMessage(anecdotes[selected]);
-  }, [anecdotes, selected]);
+    setMessage(props.anecdotes[selected][0]);
+  }, [props.anecdotes, selected]);
 
   return (
     <div>
-      <h2> {message}</h2>
-      <button onClick={() => updateMessage()}>Get Quote</button>
+      <h2>{message}</h2>
+      <h3>{vote}</h3>
+      <Button handleClick={() => setCopy()} text={'Vote'} />
+      <Button handleClick={() => setSelected(rand)} text={'Next Quote'} />
     </div>
   );
 };
+const anecdotes = [
+  ['If it hurts, do it more often', 0],
+  [
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    0,
+  ],
+  ['Premature optimization is the root of all evil.', 0],
+  [
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    0,
+  ],
+  [
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    0,
+  ],
+  ['Adding manpower to a late software project makes it later!', 0],
+];
+
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
