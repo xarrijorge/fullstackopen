cd395c49693d1c49bc5c91cc7dafd4b260eb5fa2
Xarri Jorge <kodinglife@gmail.com>
2019-11-12 01:33:38 +0000

    added disable toggle logic to button

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,45 +1,66 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
     {
-      name: "Arto Hellas"
+      name: "Arto Hellas",
+      tel: "040-1234567"
     }
   ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  let buttonStatus = newName == "" || newNumber == "" ? true : false;
 
   const handleName = event => {
     setNewName(event.target.value);
   };
 
-  let people = persons.map((person, i) => <p key={i}> {person.name} </p>);
+  const handleNumber = event => {
+    setNewNumber(event.target.value);
+  };
+
+  let people = persons.map((person, i) => (
+    <p key={i}>
+      {" "}
+      {person.name} {person.tel}
+    </p>
+  ));
 
   const handlePersons = event => {
     event.preventDefault();
     let person = {
-      name: newName
+      name: newName,
+      tel: newNumber
     };
 
     let duplicate = persons.some(elem => newName === elem.name);
     if (duplicate) {
-      alert(`${newName} is already in the phonebook`);
+      alert(`${newName} is already added to the phonebook`);
     } else {
       setPersons(persons.concat(person));
       setNewName("");
     }
   };
-
   return (
     <Fragment>
-      <h2> Phonebook </h2>{" "}
+      <h2> Phonebook </h2>
       <form onSubmit={handlePersons}>
         <div>
-          name: <input value={newName} onChange={handleName} />{" "}
-        </div>{" "}
+          <p>
+            name: <input type="text" value={newName} onChange={handleName} />
+          </p>
+          <p>
+            number:
+            <input type="tel" value={newNumber} onChange={handleNumber} />
+          </p>
+        </div>
         <div>
-          <button type="submit"> add </button>{" "}
-        </div>{" "}
-      </form>{" "}
+          <button type="submit" disabled={buttonStatus}>
+            add
+          </button>
+        </div>
+      </form>
       <h2> Numbers </h2> {people} <div></div>
     </Fragment>
   );
