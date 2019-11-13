dad4c4b05a6433c7ad4a2e7b3c84771c9ba0cf91
Xarri Jorge <kodinglife@gmail.com>
2019-11-13 00:59:44 +0000

    Add initial set of code for 'countries' optional exercises

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,12 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 
 import Search from "./components/search";
 import AddContact from "./components/addContact";
 import ShowContacts from "./components/showContacts";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filtered, setFilterd] = useState([]);
@@ -55,6 +51,14 @@ const App = () => {
       setNewName("");
     }
   };
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then(res => {
+      console.log(res);
+      setPersons(res.data);
+    });
+  }, []);
+
   return (
     <Fragment>
       <h2> Phonebook </h2>
