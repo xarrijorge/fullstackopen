c7cd69cbb8244baf95d3e84a3aa913a4a80deb43
Xarri Jorge <kodinglife@gmail.com>
2019-11-30 12:07:38 +0000

    changed basedUrl to heroku backend

--- part3/phonebook/client/src/services/contacts.js
+++ part3/phonebook/client/src/services/contacts.js
@@ -1,21 +1,21 @@
-import axios from 'axios';
-const baseUrl = 'http://localhost:3001/persons';
+import axios from "axios";
+const baseUrl = "https://floating-bayou-11024.herokuapp.com/api/persons";
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data);
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
 };
 
-const create = (newObject) => {
-    const request = axios.post(baseUrl, newObject);
-    return request.then(response => response.data);
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then(response => response.data);
 };
 const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject);
-    return request.then(response => response.data);
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then(response => response.data);
 };
-const deleteOne = (id) => {
-    axios.delete(`${baseUrl}/${id}`);
+const deleteOne = id => {
+  axios.delete(`${baseUrl}/${id}`);
 };
 
-export default { getAll, create, update, deleteOne}
+export default { getAll, create, update, deleteOne };
