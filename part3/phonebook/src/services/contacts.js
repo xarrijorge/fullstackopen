a0e5ad2caca17f4305487eafe0dbc62e92cd4827
Xarri Jorge <kodinglife@gmail.com>
2019-12-01 00:14:24 +0000

    changed baseUrl to /api/persons

--- part3/phonebook/src/services/contacts.js
+++ part3/phonebook/src/services/contacts.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'https://floating-bayou-11024.herokuapp.com/api/persons';
+const baseUrl = '/api/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
