e5ab74200fb66fbaf0ee3211c317863fc8ec3bbd
Xarri Jorge <kodinglife@gmail.com>
2019-12-01 00:01:48 +0000

    Minor updates

--- part3/phonebook/src/services/contacts.js
+++ part3/phonebook/src/services/contacts.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-const baseUrl = "/api/persons";
+import axios from 'axios';
+const baseUrl = 'https://floating-bayou-11024.herokuapp.com/api/persons';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
